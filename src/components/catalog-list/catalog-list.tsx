757f6eae3c34e54aa9a2eb044a6aa24d15aa0f90
StasVenger <stasvenger1@gmail.com>
2024-05-18 17:35:19 +0600

    Added components for card and list.

---
+++ src/components/catalog-list/catalog-list.tsx
@@ -0,0 +1,12 @@
+import CatalogCard from '@components/catalog-card/catalog-card';
+
+
+function CatalogList() {
+  return (
+    <div className="cards catalog__cards">
+      <CatalogCard />
+    </div>
+  );
+}
+
+export default CatalogList;
